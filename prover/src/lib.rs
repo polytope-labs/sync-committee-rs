37f93af32ef439ec545ff30d4c7a9b22adcdb719
David Salami <wizdave97@gmail.com>
2023-10-04 22:48:48 +0100

    nit

--- prover/src/lib.rs
+++ prover/src/lib.rs
@@ -245,7 +245,7 @@ impl SyncCommitteeProver {
 		loop {
 			// If we get to an epoch that is less than the finalized epoch for the notification
 			if compute_epoch_at_slot(block.slot) <= finality_checkpoint.epoch {
-				debug!(target: "prover", "Signature block search has reached epoch  <= finalized epoch {} finalized+2{}", compute_epoch_at_slot(block.slot), finality_checkpoint.epoch);
+				debug!(target: "prover", "Signature block search has reached epoch  <= block epoch {} finalized_epoch {}", compute_epoch_at_slot(block.slot), finality_checkpoint.epoch);
 				return Ok(None)
 			}
 
